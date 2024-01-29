aa251d9285269a9db487f0c70f0dca7346058a8f
Jerry Wang <ziyanwang1105@gmail.com>
2024-01-29 15:00:04 -0800

    finish children node and losing node

--- lib/tic_tac_toe_node.rb
+++ lib/tic_tac_toe_node.rb
@@ -1,10 +1,33 @@
 require_relative 'tic_tac_toe'
 
+
+
 class TicTacToeNode
+
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
+
+
   end
 
   def losing_node?(evaluator)
+    if board.over?
+      return board.won? && board.winner != evaluator
+    end
+    if self.next_mover_mark == evaluator
+      self.children.all? do |node|
+        node.losing_node?(evaluator)
+      end
+    else
+      self.children.any? do |node|
+        node.losing_node?(node.next_mover_mark)
+      end
+    end
+
   end
 
   def winning_node?(evaluator)
@@ -13,5 +36,20 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    arr = []
+    (0..2).each do |i|
+      (0..2).each do |j|
+        pos = [i, j]
+        board_dup = board.dup
+        if board_dup.empty?(pos)
+          board_dup[pos] = self.next_mover_mark
+          mark = (self.next_mover_mark == :x ? :o : :x)
+          arr << TicTacToeNode.new(board_dup, mark, pos)
+        end
+      end
+    end
+    arr
+
+
   end
 end
